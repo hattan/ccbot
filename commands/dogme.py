c113c80be8ae8bfff8b60cd16116fd0f7d4e8307
Hattan Shobokshi <hattan.shobokshi@verizondigitalmedia.com>
2018-01-04 19:12:56 -0800

    dogme cleanup and added goatme command

--- commands/dogme.py
+++ commands/dogme.py
@@ -1,9 +1,30 @@
+import urllib2
+import json
+import random
+
 class DogMe:
+    dogs_url = "https://www.reddit.com/r/puppies.json"
+    dogs_cache = []
     def get_channel_id(self):
         return "all"
 
     def invoke(self):
-        return "woof woof" , None
+        if not self.dogs_cache:
+            req = urllib2.Request(self.dogs_url)
+            req.add_header('User-Agent', 'codecamp-bot')
+            resp = urllib2.urlopen(req)
+            data = json.loads(resp.read())
+            for child in data['data']['children']:
+                images = child['data']['preview']['images']
+                self.dogs_cache.append(images[0]['source']['url'])
+        image_url = random.choice(self.dogs_cache)
+        attachments = attachments = [{"title": image_url, "image_url": image_url}]
+        return None,attachments
 
     def get_command(self):
         return "dogme"
+
+if __name__ == "__main__":
+    dogme = DogMe()
+    text,attachments = dogme.invoke()
+    print attachments
---
+++ commands/goatme.py
@@ -0,0 +1,31 @@
+import os
+import urllib2
+import json
+import random
+
+class GoatMe:
+    url = "https://api.imgur.com/3/gallery/r/babygoats"
+    imgur_key = os.environ.get('IMGUR_CLIENT_ID')
+    cache = []
+    def get_channel_id(self):
+        return "all"
+
+    def invoke(self):
+        if not self.cache:
+            req = urllib2.Request(self.url)
+            req.add_header('User-Agent', 'codecamp-bot')
+            req.add_header('Authorization','Client-ID ' + self.imgur_key)
+            resp = urllib2.urlopen(req)
+            data = json.loads(resp.read())
+            for child in data['data']:
+                if 'images' in child:
+                    self.cache.append(child['images'][0]['link'])
+                else:
+                     self.cache.append(child['link'])
+        image_url = random.choice(self.cache)
+        attachments = attachments = [{"title": image_url, "image_url": image_url}]
+        return None,attachments
+
+    def get_command(self):
+        return "goatme"
+
