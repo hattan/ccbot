47f4d64c00fd90d6d1d6f6f14970946c2e88b5de
Hattan Shobokshi <hattan.shobokshi@verizondigitalmedia.com>
2018-01-04 16:21:11 -0800

    initial commit

---
+++ commands/catme.py
@@ -0,0 +1,23 @@
+import urllib
+import json
+import random
+
+class CatMe:
+    cats_url = "https://api.github.com/repos/flores/moarcats/contents/cats?ref=master"
+    cats_cache = None
+
+    def get_channel_id(self):
+        return "all"
+
+    def invoke(self):
+        if self.cats_cache is None:
+            response = urllib.urlopen(self.cats_url)
+            data = json.loads(response.read())
+            self.cats_cache = data
+        image = random.choice(self.cats_cache)["name"]
+        image_url = "http://edgecats.net/cats/" + image
+        attachments = attachments = [{"title": image_url, "image_url": image_url}]
+        return None,attachments
+
+    def get_command(self):
+        return "catme"
---
+++ commands/dogme.py
@@ -0,0 +1,9 @@
+class DogMe:
+    def get_channel_id(self):
+        return "all"
+
+    def invoke(self):
+        return "woof woof" , None
+
+    def get_command(self):
+        return "dogme"
