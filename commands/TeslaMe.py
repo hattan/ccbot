ccb83328145c7b40520d621727ef3b98417abad2
Hattan Shobokshi <hattan@gmail.com>
2018-01-22 14:44:14 -0800

    added aww and teslame commands

---
+++ commands/TeslaMe.py
@@ -0,0 +1,27 @@
+import urllib2
+import json
+import random
+
+class TeslaMe:
+    url = "https://www.reddit.com/r/TeslaPorn.json"
+    cache = []
+    def get_channel_id(self):
+        return "all"
+
+    def invoke(self, command, user):
+        if not self.cache:
+            req = urllib2.Request(self.url)
+            req.add_header('User-Agent', 'codecamp-bot')
+            resp = urllib2.urlopen(req)
+            data = json.loads(resp.read())
+            for child in data['data']['children']:
+                if 'preview' in child['data']:
+                    images = child['data']['preview']['images']
+                    self.cache.append(images[0]['source']['url'])
+        image_url = random.choice(self.cache)
+        attachments = attachments = [{"title": image_url, "image_url": image_url}]
+        return None,attachments
+
+    def get_command(self):
+        return "teslame"
+
