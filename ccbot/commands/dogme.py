444cceee2f2b5a150c79a98dfb0469999769f979
Hattan Shobokshi <hattan@gmail.com>
2018-01-27 16:38:08 -0800

    refactored Dogme command

--- ccbot/commands/dogme.py
+++ ccbot/commands/dogme.py
@@ -6,14 +6,18 @@ from services.reddit_client import RedditApiClient
 
 class DogMe():
     url = "https://www.reddit.com/r/puppies.json"
-    
+    reddit_client = None
+
     def get_channel_id(self):
         return "all"
 
     def invoke(self, command, user):
-        reddit_client = RedditApiClient(self.url)
-        attachments = reddit_client.fetch()
+        if self.reddit_client is None:
+            self.reddit_client = RedditApiClient(self.url)
+
+        attachments = self.reddit_client.fetch()
         return None,attachments
+        
 
     def get_command(self):
         return "dogme"
