8e142fe51ae439d4d3b788eade50d93111ec8a98
Hattan Shobokshi <hattan@gmail.com>
2018-01-27 16:51:01 -0800

    refacted catme to use api_client

--- ccbot/commands/catme.py
+++ ccbot/commands/catme.py
@@ -1,18 +1,23 @@
 import urllib
 import json
 import random
+from services.api_client import ApiClient
 
 class CatMe:
     cats_url = "https://api.github.com/repos/flores/moarcats/contents/cats?ref=master"
+    api_client = None
     cats_cache = None
+
+    def __init__(self):
+        self.api_client = ApiClient()
+        print "invoked"
 
     def get_channel_id(self):
         return "all"
 
     def invoke(self, command, user):
         if self.cats_cache is None:
-            response = urllib.urlopen(self.cats_url)
-            data = json.loads(response.read())
+            data = self.api_client.fetch(self.cats_url)
             self.cats_cache = data
         image = random.choice(self.cats_cache)["name"]
         image_url = "http://edgecats.net/cats/" + image
--- ccbot/services/api_client.py
+++ ccbot/services/api_client.py
@@ -5,7 +5,7 @@ class ApiClient:
     user_agent = "codecamp-bot"
 
     def fetch(self,url):
-        req = urllib2.Request(self.url)
+        req = urllib2.Request(url)
         req.add_header('User-Agent', self.user_agent)
         resp = urllib2.urlopen(req)
         data = json.loads(resp.read())
