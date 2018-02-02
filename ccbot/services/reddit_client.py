52f5bdbe50ea46b9ddb32037bdd370f3a78713c4
Hattan Shobokshi <hattan@gmail.com>
2018-02-02 14:19:12 -0800

    removed caching from reddit_client and added memoization util

--- ccbot/services/reddit_client.py
+++ ccbot/services/reddit_client.py
@@ -1,16 +1,16 @@
 import random
 
 from api_client import ApiClient
+from utils.cache import memoize
 
 
 class RedditApiClient(ApiClient):
-    use_cache = None
-    cache = []
     def __init__(self, url='', use_cache=True):
-        self.use_cache = use_cache
         self.url = url
 
+    @memoize
     def get_data(self):
+        print "fetched"
         result = []
         data = ApiClient.fetch(self,self.url)
         for child in data['data']['children']:
@@ -20,20 +20,14 @@ class RedditApiClient(ApiClient):
         return result
 
     def fetch(self):
-        if self.use_cache:
-            if len(self.cache) == 0:
-                self.cache = self.get_data()
-            data = self.cache
-        else:
-            data = self.get_data()
-
+        data = self.get_data()
         image_url = random.choice(data)
         attachments = attachments = [{"title": image_url, "image_url": image_url}]
         return attachments
    
 
 if __name__ == "__main__":
-    client = RedditApiClient("https://www.reddit.com/r/aww.json",use_cache=True)
+    client = RedditApiClient("https://www.reddit.com/r/aww.json")
     attachment = client.fetch()
     print attachment
 
---
+++ ccbot/utils/cache.py
@@ -0,0 +1,13 @@
+from functools import wraps
+
+def memoize(function):
+    memo = {}
+    @wraps(function)
+    def wrapper(*args):
+        if args in memo:
+            return memo[args]
+        else:
+            rv = function(*args)
+            memo[args] = rv
+            return rv
+    return wrapper
