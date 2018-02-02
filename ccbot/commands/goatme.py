1505c40c988a4589304fa6b4b3da5f01cd4f279e
Hattan Shobokshi <hattan@gmail.com>
2018-02-02 14:25:07 -0800

    added memorization to goatme command and removed manual caching

--- ccbot/commands/goatme.py
+++ ccbot/commands/goatme.py
@@ -2,11 +2,11 @@ import os
 import random
 from services.api_client import ApiClient
 from services.slack_response import SlackResponse
+from utils.cache import memoize
 
 class GoatMe:
     url = "https://api.imgur.com/3/gallery/r/babygoats"
     imgur_key = os.environ.get('IMGUR_CLIENT_ID')
-    cache = []
     api_client = None
 
     def __init__(self):
@@ -14,19 +14,22 @@ class GoatMe:
 
     def get_channel_id(self):
         return "all"
-
-    def invoke(self, command, user):
-        if not self.cache:
-            headers = {'Authorization' : 'Client-ID ' + self.imgur_key}
-            data = self.api_client.fetch(self.url,headers)
-            for child in data['data']:
-                if 'images' in child:
-                    self.cache.append(child['images'][0]['link'])
-                else:
-                     self.cache.append(child['link'])
-
-        image_url = random.choice(self.cache)
+    
+    @memoize
+    def get_data(self):
+        response = []
+        headers = {'Authorization' : 'Client-ID ' + self.imgur_key}
+        data = self.api_client.fetch(self.url,headers)
+        for child in data['data']:
+            if 'images' in child:
+                response.append(child['images'][0]['link'])
+            else:
+                response.append(child['link'])
+        return response
         
+    def invoke(self, command, user):
+        data = self.get_data()
+        image_url = random.choice(data)
         return SlackResponse.attachment(title=image_url,image_url=image_url)
 
     def get_command(self):
