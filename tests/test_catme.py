e16d3cbc42e81a3b3c5f62eb5ddb9c5227d4f5e0
Hattan Shobokshi <hattan@gmail.com>
2018-02-03 19:15:34 -0800

    added catme mock tests

--- tests/test_catme.py
+++ tests/test_catme.py
@@ -14,3 +14,19 @@ def test_catme_commandtext_is_catme():
 
 def test_catme_available_in_all_channels():
     assert CatMe().get_channel_id() == "all"  
+
+def test_catme_invoke_calls_api_client_fetch_returns_edgecats_url():
+    api_client = ApiClient()
+    api_client.fetch = MagicMock(return_value=[{"name" : "foo"}])
+    catme = CatMe()
+    catme.api_client=api_client
+    text,attachments = catme.invoke("catme","fakeuser")
+    assert attachments == [{"title": "http://edgecats.net/cats/foo", "image_url": "http://edgecats.net/cats/foo"}]      
+
+def test_catme_invoke_calls_api_client_fetch_text_is_none():
+    api_client = ApiClient()
+    api_client.fetch = MagicMock(return_value=[{"name" : "foo"}])
+    catme = CatMe()
+    catme.api_client=api_client
+    text,attachments = catme.invoke("catme","fakeuser")
+    assert text is None
