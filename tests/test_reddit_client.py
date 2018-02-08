21a2c731d9292a37b2717cacb8cee823ccf85826
Hattan Shobokshi <hattan@gmail.com>
2018-02-08 15:56:46 -0800

    added reddit_client test for parsing api response

--- tests/test_reddit_client.py
+++ tests/test_reddit_client.py
@@ -6,13 +6,23 @@ from mock_datetime import mock_datetime
 from mock import MagicMock,patch
 
 @patch('ccbot.services.api_client.ApiClient.fetch')
-def test_get_data(fake_fetch):
+def test_get_data_calls_api_client_fetch(fake_fetch):
     #arrange
     reddit = RedditApiClient("fake_url")
     #act
     data = reddit.get_data()
     #assert
     fake_fetch.assert_called_with(reddit,"fake_url")
+
+@patch('ccbot.services.api_client.ApiClient.fetch')
+def test_get_data_extracts_images_from_reddit_response(fake_fetch):
+    #arrange
+    reddit = RedditApiClient("fake_url")
+    fake_fetch.return_value = {"data": {"children": [{"data": {"preview": {"images": [{"source": {"url": "fake_img"}}],}}},{"data": {"preview": {"images": [{"source": {"url": "fake_img2"}}],}}}]}}
+    #act
+    data = reddit.get_data()
+    #assert
+    assert data == ['fake_img','fake_img2'] 
 
 def test_fetch():
     #arrange
