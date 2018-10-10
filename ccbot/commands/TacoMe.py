8c9bd238aa738c315bb2ddd3c642e246e0bf1a97
nurih <chip.marklar@gmail.com>
2018-10-09 20:00:26 -0700

    TacoMe parses zip and returns random entry from results around that zip.

--- ccbot/commands/TacoMe.py
+++ ccbot/commands/TacoMe.py
@@ -1,12 +1,12 @@
+import re
 import os
 from services.api_client import ApiClient
 from services.slack_response import SlackResponse
 from utils.cache import *
 from urllib2 import *
 
-
 class TacoMe:
-    url = "https://api.yelp.com/v3/businesses/search?term=taco&sort_by=distance&categories=foodtrucks&location=90405"
+    url = "https://api.yelp.com/v3/businesses/search?term=taco&sort_by=distance&categories=foodtrucks&location="
     api_client = None
 
     def __init__(self):
@@ -15,6 +15,7 @@ class TacoMe:
             'Accept': 'application/json',
             'Authorization': self.get_auth_value()
         }
+        self.zipcode_re = re.compile(r'^tacome\s+(\d{5})\s*$')
 
     def get_channel_id(self):
         return "all"
@@ -23,23 +24,43 @@ class TacoMe:
         token = os.environ.get('SLACK_CODE_CAMP_BOT_YELP_TOKEN')
         return 'Bearer ' + token if token else None
 
-    def get_data(self):
-        return self.api_client.fetch(self.url, headers=self.api_headers)
+    def get_zipcode(self, command):
+        matched = self.zipcode_re.match(command)
+        return matched.group(1) if matched else None
+
+    def get_data(self, url):
+        return self.api_client.fetch(url, headers=self.api_headers)
 
     def invoke(self, command, user):
+
+        zip = self.get_zipcode(command)
+
+        if(zip is None):
+            return SlackResponse.text(r'Try: `tacome _ZIP_CODE_`. Example: `tacome 90405`')
+
+        url = self.url+zip
+        
         try:
-            result = self.get_data()
+            result = self.get_data(url)
         except HTTPError as he:
             return SlackResponse.text(str(he))
         except URLError as ue:
-            return SlackResponse.text(str(e.args))
+            return SlackResponse.text(str(ue.args))
 
         if (result['total'] == 0):
             return SlackResponse.text('Sorry, no taco for ' + command)
 
-        found = result['businesses'][0]
-        description = 'Rated %s by %s people, is %s meters away price level %s' % (
-            found['rating'], found['review_count'], found['distance'], found['price'])
+        return self.create_slack_response(result)
+
+    def create_slack_response(self, api_response):
+        found = random.choice(api_response['businesses'])
+        
+        description = 'Rated %s by %s people, is %s meters away' % (
+            found['rating'],
+            found['review_count'],
+            int(found['distance'])
+            )
+
         return SlackResponse.attachment(
             title=found['name'],
             image_url=found['image_url'],
--- tests/test_tacome.py
+++ tests/test_tacome.py
@@ -6,22 +6,51 @@ sys.path.append("ccbot")
 from ccbot.commands.TacoMe import TacoMe
 from ccbot.services.api_client import *
 from services.slack_response import SlackResponse
-from mock import MagicMock,patch
+from mock import MagicMock, patch
 
 COMMAND = 'tacome 90405'
 
+
 def test_commandtext_is_tacome():
     assert TacoMe().get_command() == "tacome"
+
 
 def test_available_in_all_channels():
     assert TacoMe().get_channel_id() == "all"
 
 
-def test_invoke_returns_title():
+def test_invoke_without_zip_returns_suggestion():
+    result = TacoMe().invoke("tacome", "fake_user")
+    assert result[0] == 'Try: `tacome _ZIP_CODE_`. Example: `tacome 90405`'
+
+
+
+def test_invoke_returns_attachment():
     result = TacoMe().invoke(COMMAND, "fake_user")
     for entry in ('title', 'image_url', 'text'):
         assert result[1][0][entry]
 
+
 def test_get_auth_value_returns_bearer():
     result = TacoMe().get_auth_value()
     assert result.startswith('Bearer ')
+
+
+def test_get_zipcode_extracts_5digits():
+    cases = {
+        'tacome': None,
+        'tacome 12345': '12345',
+        'tacome   12345': '12345',
+        'tacome   12345   ': '12345',
+        'tacome 1234': None,
+        'tacome 123456': None,
+        'tacome a 123456': None,
+
+    }
+    
+    for command,expected in cases.iteritems():
+        actual = TacoMe().get_zipcode(command)
+        if (expected is None):
+                assert actual is None
+        else:
+                assert actual == expected 
