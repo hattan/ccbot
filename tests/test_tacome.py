1d4e7620748dcda81f28fbc46f6dd15a404f86fc
nurih <chip.marklar@gmail.com>
2018-10-10 10:17:21 -0700

    remove integration test. add mocked api call, test for api repsonse parsing.

--- tests/test_tacome.py
+++ tests/test_tacome.py
@@ -5,52 +5,65 @@ sys.path.append("ccbot")
 
 from ccbot.commands.TacoMe import TacoMe
 from ccbot.services.api_client import *
-from services.slack_response import SlackResponse
+from ccbot.services.slack_response import SlackResponse
 from mock import MagicMock, patch
+import pytest
+import unittest
 
-COMMAND = 'tacome 90405'
-
-
-def test_commandtext_is_tacome():
-    assert TacoMe().get_command() == "tacome"
-
-
-def test_available_in_all_channels():
-    assert TacoMe().get_channel_id() == "all"
-
-
-def test_invoke_without_zip_returns_suggestion():
-    result = TacoMe().invoke("tacome", "fake_user")
-    assert result[0] == 'Try: `tacome _ZIP_CODE_`. Example: `tacome 90405`'
+ZIP = '90405'
+COMMAND = 'tacome ' + ZIP
+API_RESPONSE_SAMPLE = {'businesses': [
+    {
+        'name': 'marklar', 
+        'image_url': 'https://taco.pic/1', 
+        'rating': 1, 
+        'review_count': 2, 
+        'distance': 3}]}
 
 
+class TacoMeTest(unittest.TestCase):
 
-def test_invoke_returns_attachment():
-    result = TacoMe().invoke(COMMAND, "fake_user")
-    for entry in ('title', 'image_url', 'text'):
-        assert result[1][0][entry]
+    def test_commandtext_is_tacome(self):
+        assert TacoMe().get_command() == "tacome"
 
+    def test_available_in_all_channels(self):
+        assert TacoMe().get_channel_id() == "all"
 
-def test_get_auth_value_returns_bearer():
-    result = TacoMe().get_auth_value()
-    assert result.startswith('Bearer ')
+    def test_invoke_without_zip_returns_suggestion(self):
+        result = TacoMe().invoke("tacome", "fake_user")
+        assert result[0] == 'Try: `tacome _ZIP_CODE_`. Example: `tacome 90405`'
 
+    @patch.object(TacoMe, 'get_data')
+    def test_invoke_calls_get_data(self, get_data):
+        get_data.return_value = {'total': 0}
+        TacoMe().invoke(COMMAND, "fake_user")
+        expected = "https://api.yelp.com/v3/businesses/search?term=taco&sort_by=distance&categories=foodtrucks&location=" + ZIP
 
-def test_get_zipcode_extracts_5digits():
-    cases = {
-        'tacome': None,
-        'tacome 12345': '12345',
-        'tacome   12345': '12345',
-        'tacome   12345   ': '12345',
-        'tacome 1234': None,
-        'tacome 123456': None,
-        'tacome a 123456': None,
+        get_data.assert_called_with(expected)
 
-    }
-    
-    for command,expected in cases.iteritems():
-        actual = TacoMe().get_zipcode(command)
-        if (expected is None):
+    def test_create_slack_response_populates_attachment(self):
+        actual = TacoMe().create_slack_response(API_RESPONSE_SAMPLE)
+        for entry in ('title', 'image_url', 'text'):
+            assert actual[1][0][entry]
+
+    def test_get_auth_value_returns_bearer(self):
+        result = TacoMe().get_auth_value()
+        assert result.startswith('Bearer ')
+
+    def test_get_zipcode_extracts_5digits(self):
+        cases = {
+            'tacome': None,
+            'tacome 12345': '12345',
+            'tacome   12345': '12345',
+            'tacome   12345   ': '12345',
+            'tacome 1234': None,
+            'tacome 123456': None,
+            'tacome a 123456': None
+        }
+
+        for command, expected in cases.iteritems():
+            actual = TacoMe().get_zipcode(command)
+            if (expected is None):
                 assert actual is None
-        else:
-                assert actual == expected 
+            else:
+                assert actual == expected
