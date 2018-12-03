40ed5fe49fa160c5caa22a82d6e74f59e456fe85
Hattan Shobokshi <hattan@gmail.com>
2018-12-03 09:10:07 -0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1dev',
     description='SoCal Code Camp bot',
     url='http://github.com/hattan/ccbot',    
-    packages=['ccbot'],
+    packages=['ccbot','ccbot/commands','ccbot/services','ccbot/utils'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
