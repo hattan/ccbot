f520d9a7625017e29be10fc34cbe0ed80c6da908
root <root@Hattan-Surface.northamerica.corp.microsoft.com>
2018-12-03 09:02:18 -0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ccbot',
+    version='0.1dev',
+    description='SoCal Code Camp bot',
+    url='http://github.com/hattan/ccbot',    
+    packages=['ccbot'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
